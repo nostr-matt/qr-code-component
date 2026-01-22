51bc0f0285dd10ebb3a6d54cd3634c3a6f7526a9
Matt <nostro_matt@pm.me>
2026-01-22 16:53:13 +0700

    Revised headings

--- resources/solution-template.js
+++ resources/solution-template.js
@@ -17,7 +17,15 @@ export class SolutionTemplate extends HTMLBodyElement {
             }
 
             main > h1 {
-                display: none;
+                position: absolute;
+                width: 1px;
+                height: 1px;
+                margin: -1px;
+                padding: 0;
+                overflow: hidden;
+                clip: rect(0 0 0 0);
+                white-space: nowrap;
+                border: 0;
             }
 
             :host([center-contents]) main {
